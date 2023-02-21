9439619762a0118a40e6038981c277bdc2b021b8
Aidan <iiiaiid4niii@gmail.com>
2023-02-21 08:47:07 +0000

    attempting to make error controllers work for incorrect endppoints

--- controllers/errorControllers.js
+++ controllers/errorControllers.js
@@ -1,8 +1,8 @@
 exports.handle500Errors = (error, req, res, next) => {
-    console.log(error)
-    res.status(500).send({msg: "Server Error"})
-}
+  console.log(error);
+  res.status(500).send({ msg: "Server Error" });
+};
 
 exports.handle404Errors = (error, req, res, next) => {
-    res.status(404).send({msg: "incorrect endpoint"})
-}
+  res.status(405).send({ msg: "incorrect endpoint" });
+};
--- models/comments-models.js
+++ models/comments-models.js
@@ -1,8 +1,21 @@
 const db = require("../db/connection");
 
 exports.getReviewsComments = (review_id) => {
-  return db.query(
-    `SELECT * FROM comments WHERE review_id = $1 ORDER BY created_at DESC;`,
-    [review_id]
-  );
+  if (isNaN(Number(review_id)) === true) {
+    return Promise.reject({ status: 400, msg: "bad request" });
+  } else {
+    return db
+      .query(
+        `SELECT * FROM comments WHERE review_id = $1 ORDER BY created_at DESC;`,
+        [review_id]
+      )
+      .then((response) => {
+        const arr = response.rows;
+        if (arr.length === 0) {
+          return Promise.reject({ status: 404, msg: "id does not exist" });
+        } else {
+          return response;
+        }
+      });
+  }
 };
