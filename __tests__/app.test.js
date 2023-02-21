be8bf16ca8f64a51c471046664fe8ce4ec62e621
Aidan <iiiaiid4niii@gmail.com>
2023-02-21 14:39:00 +0000

    added error msg checking to tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,147 +1,153 @@
-const request = require("supertest")
-const db = require("../db/connection")
-const app = require("../app")
-const data = require("../db/data/test-data/index")
-const seed = require("../db/seeds/seed")
- 
+const request = require("supertest");
+const db = require("../db/connection");
+const app = require("../app");
+const data = require("../db/data/test-data/index");
+const seed = require("../db/seeds/seed");
 
 beforeEach(() => {
-    return seed(data)
-})
+  return seed(data);
+});
 
 afterAll(() => {
-    return db.end()
-})
-
-
-describe('200: GET /api/categories', () => {
-    it('should respond with an array of categories', () => {
-        return request(app)
-        .get("/api/categories")
-        .expect(200)
-        .then((response) => {
-            expect(Array.isArray(response.body.categories)).toBe(true)
-        })
-    });
-    it('response array should be the correct length as the data', () => {
-        return request(app)
-            .get("/api/categories")
-            .expect(200)
-            .then((response) => {
-                const arr = response.body.categories
-                expect(arr.length).toBe(4)
-            })
-    });
-    it('each item in the returned array should contain a key of slug and description ', () => {
-        return request(app)
-            .get("/api/categories")
-            .expect(200)
-            .then((response) => {
-                const arr = response.body.categories
-                arr.forEach((category) => {
-                    expect(category).toMatchObject({
-                        slug: expect.any(String),
-                        description: expect.any(String),
-                    })
-                })
-            })
-    });
+  return db.end();
 });
 
-describe('200: GET /api/reviews', () => {
-    it('should return an array of all review objects', () => {
-        return request(app)
-            .get("/api/reviews")
-            .expect(200)
-            .then((response) => {
-                expect(response.body.reviews).toBeInstanceOf(Array)
-            })
-    });
-    it('response array should match the length of the test data', () => {
-        return request(app)
-            .get("/api/reviews")
-            .expect(200)
-            .then((response) => {
-                const arr = response.body.reviews
-                expect(arr.length).toBe(13)
-            })
-    });
-    it('each item in the returned array should contain the correct keys', () => {
-        return request(app)
-            .get("/api/reviews")
-            .expect(200)
-            .then((response) => {
-                const arr = response.body.reviews
-                arr.forEach((review) => {
-                    expect(review).toMatchObject({
-                        owner: expect.any(String),
-                        title: expect.any(String),
-                        review_id: expect.any(Number),
-                        category: expect.any(String),
-                        review_img_url: expect.any(String),
-                        created_at: expect.any(String),
-                        votes: expect.any(Number),
-                        designer: expect.any(String),
-                        comment_count: expect.any(Number),
-                    })
-                })
-            })
-    });
-    it('should return the reviews sorted in date descending order', () => {
-        return request(app)
-        .get("/api/reviews")
-        .expect(200)
-        .then((response) => {
-            const arr = response.body.reviews
-            expect(arr).toBeSortedBy('created_at', {descending: true})
-        })    
-    });
+describe("200: GET /api/categories", () => {
+  it("should respond with an array of categories", () => {
+    return request(app)
+      .get("/api/categories")
+      .expect(200)
+      .then((response) => {
+        expect(Array.isArray(response.body.categories)).toBe(true);
+      });
+  });
+  it("response array should be the correct length as the data", () => {
+    return request(app)
+      .get("/api/categories")
+      .expect(200)
+      .then((response) => {
+        const arr = response.body.categories;
+        expect(arr.length).toBe(4);
+      });
+  });
+  it("each item in the returned array should contain a key of slug and description ", () => {
+    return request(app)
+      .get("/api/categories")
+      .expect(200)
+      .then((response) => {
+        const arr = response.body.categories;
+        arr.forEach((category) => {
+          expect(category).toMatchObject({
+            slug: expect.any(String),
+            description: expect.any(String),
+          });
+        });
+      });
+  });
 });
 
-describe('200: GET api/reviews/:review_id', () => {
-    it('should respond with a single review object', () => {
-        return request(app)
-            .get("/api/reviews/3")
-            .expect(200)
-            .then((response) => {
-                const arr = response.body.review
-                expect(arr.length).toBe(1)
-            })
-    });
-    it('the returned object should contain all the correct keys', () => {
-        return request(app)
-            .get("/api/reviews/3")
-            .expect(200)
-            .then((response) => {
-                const arr = response.body.review
-                expect(arr[0]).toMatchObject({
-                        review_id: expect.any(Number),
-                        owner: expect.any(String),
-                        title: expect.any(String),
-                        category: expect.any(String),
-                        review_img_url: expect.any(String),
-                        created_at: expect.any(String),
-                        votes: expect.any(Number),
-                        designer: expect.any(String),
-                        review_body: expect.any(String),
-                })
-            })
-    });
+describe("200: GET /api/reviews", () => {
+  it("should return an array of all review objects", () => {
+    return request(app)
+      .get("/api/reviews")
+      .expect(200)
+      .then((response) => {
+        expect(response.body.reviews).toBeInstanceOf(Array);
+      });
+  });
+  it("response array should match the length of the test data", () => {
+    return request(app)
+      .get("/api/reviews")
+      .expect(200)
+      .then((response) => {
+        const arr = response.body.reviews;
+        expect(arr.length).toBe(13);
+      });
+  });
+  it("each item in the returned array should contain the correct keys", () => {
+    return request(app)
+      .get("/api/reviews")
+      .expect(200)
+      .then((response) => {
+        const arr = response.body.reviews;
+        arr.forEach((review) => {
+          expect(review).toMatchObject({
+            owner: expect.any(String),
+            title: expect.any(String),
+            review_id: expect.any(Number),
+            category: expect.any(String),
+            review_img_url: expect.any(String),
+            created_at: expect.any(String),
+            votes: expect.any(Number),
+            designer: expect.any(String),
+            comment_count: expect.any(Number),
+          });
+        });
+      });
+  });
+  it("should return the reviews sorted in date descending order", () => {
+    return request(app)
+      .get("/api/reviews")
+      .expect(200)
+      .then((response) => {
+        const arr = response.body.reviews;
+        expect(arr).toBeSortedBy("created_at", { descending: true });
+      });
+  });
 });
 
-describe('404:', () => {
+describe("200: GET api/reviews/:review_id", () => {
+  it("should respond with a single review object", () => {
+    return request(app)
+      .get("/api/reviews/3")
+      .expect(200)
+      .then((response) => {
+        const arr = response.body.review;
+        expect(arr.length).toBe(1);
+      });
+  });
+  it("the returned object should contain all the correct keys", () => {
+    return request(app)
+      .get("/api/reviews/3")
+      .expect(200)
+      .then((response) => {
+        const arr = response.body.review;
+        expect(arr[0]).toMatchObject({
+          review_id: expect.any(Number),
+          owner: expect.any(String),
+          title: expect.any(String),
+          category: expect.any(String),
+          review_img_url: expect.any(String),
+          created_at: expect.any(String),
+          votes: expect.any(Number),
+          designer: expect.any(String),
+          review_body: expect.any(String),
+        });
+      });
+  });
+});
+
+describe("404:", () => {
   test("should respond with a 404 msg when the user enters a review id that is not in the database", () => {
-    return request(app).get("/api/reviews/20").expect(404);
+    return request(app).get("/api/reviews/20").expect(404).then(({body}) => {
+      expect(body.msg).toBe("review id does not exist")
+    })
   });
   test("should give a 404 error when an incorrect endpoint is provided", () => {
-    return request(app)
-      .get("/aasdjaisdj")
-      .expect(404)
+    return request(app).get("/aasdjaisdj").expect(404).then(({body}) => {
+      expect(body.msg).toBe("incorrect endpoint")
+    })
   });
 });
 
 describe("400 bad request incorrect id type", () => {
   test("should return a 400 error if user does not provide a number to the endpoint", () => {
-    return request(app).get("/api/reviews/aidansreview").expect(400);
+    return request(app)
+      .get("/api/reviews/aidansreview")
+      .expect(400)
+      .then((response) => {
+        const msg = response.body.msg
+        expect(msg).toBe("bad request");
+      });
   });
 });
