6a46fe7b9b6be7ae12cd400dd66cdb30b5a9d38e
Aidan <iiiaiid4niii@gmail.com>
2023-02-24 10:54:28 +0000

    working on hosting api

--- db/connection.js
+++ db/connection.js
@@ -1,12 +1,23 @@
+
 const { Pool } = require('pg');
+
+
 const ENV = process.env.NODE_ENV || 'development';
 
 require('dotenv').config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
-if (!process.env.PGDATABASE) {
-  throw new Error('PGDATABASE not set');
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error('PGDATABASE or DATABASE_URL NOT SET');
 }
 
-module.exports = new Pool();
+const config = 
+  ENV === 'production'
+  ? {
+    connectionString: process.env.DATABASE_URL,
+    max: 2,
+  }
+: {}
+
+module.exports = new Pool(config);
